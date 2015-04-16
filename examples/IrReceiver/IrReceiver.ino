37f8e71f746f64806589afef5b3fde58c29458ac
Marcus Ahlberg <marcus@aahlborg.se>
2015-04-16 20:30:43 +0200

    Fix memory corruption bug
    Allocate StartModule in static memory instead of on the stack.

--- examples/IrReceiver/IrReceiver.ino
+++ examples/IrReceiver/IrReceiver.ino
@@ -1,9 +1,17 @@
-#include "StartModule.h"
+#include <StartModule.h>
 
 #define IR_PIN 20
 #define LED_PIN 13
 #define START_MODULE_EEPROM_ADDR 0x40
 
+// Create StartModule object
+static StartModule startModule(
+  IR_PIN,
+  LED_PIN,
+  START_MODULE_EEPROM_ADDR,
+  stateChange);
+
+// Callback function
 void stateChange(robot_state newState)
 {
   Serial.printf("New state: %d\n", newState);
@@ -12,15 +20,9 @@ void stateChange(robot_state newState)
 void setup()
 {
   Serial.begin(9600);
-  delay(10000);
-  StartModule startModule(IR_PIN,
-                          LED_PIN,
-                          START_MODULE_EEPROM_ADDR,
-                          stateChange);
 }
 
 void loop()
 {
   delay(1000);
 }
-
