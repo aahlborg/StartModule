cfcd51ade3b2a96d9988db2a1fd89f76f32811cd
Marcus Ahlberg <marcus@aahlborg.se>
2015-04-20 21:43:45 +0200

    Fix example program
    Initialize Serial before StartModule to prevent
    printing to an uninitialized serial port

--- examples/IrReceiver/IrReceiver.ino
+++ examples/IrReceiver/IrReceiver.ino
@@ -1,29 +1,40 @@
 #include <StartModule.h>
 #include <EEPROM.h>
+#include <IntervalTimer.h>
 
 #define IR_PIN 20
 #define LED_PIN 13
 #define START_MODULE_EEPROM_ADDR 0x40
 
-// Create StartModule object
-static StartModule startModule(
-  IR_PIN,
-  LED_PIN,
-  START_MODULE_EEPROM_ADDR,
-  stateChange);
+// StartModule object
+static StartModule * startModule;
+
+// Motor state
+int motorState = 0;
 
 // Callback function
 void stateChange(robot_state newState)
 {
-  Serial.printf("New state: %d\n", newState);
+  if (STATE_RUNNING == newState)
+    motorState = 1;
+  else
+    motorState = 0;
+
+  Serial.printf("New state: %d motors: %d\n", newState, motorState);
 }
 
 void setup()
 {
   Serial.begin(9600);
+  startModule = new StartModule(
+    IR_PIN,
+    LED_PIN,
+    START_MODULE_EEPROM_ADDR,
+    stateChange);
 }
 
 void loop()
 {
   delay(1000);
 }
+
